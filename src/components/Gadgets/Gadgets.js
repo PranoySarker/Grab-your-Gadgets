c5c56d2b7032e13afd53476f71df939de9023ae3
PranoySarker <pranoysarker01@gmail.com>
2022-03-26 02:44:30 +0600

    cart-btn added

--- src/components/Gadgets/Gadgets.js
+++ src/components/Gadgets/Gadgets.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import './Gadgets.css'
+import './Gadgets.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Gadgets = (props) => {
     console.log(props);
-    const { name, image, price } = props.gadget
+    const { name, image, price } = props.gadget;
     return (
         <div className='gadget-card'>
             <img className='gadget-img' src={image} alt="" />
-            {/* <h1>This is Gadgets:{name}</h1> */}
+            <div className='product-info'>
+                <h3>Name: {name}</h3>
+                <h4>Price: ${price}</h4>
+            </div>
+            <button className='card-btn'>Add To Cart
+                <FontAwesomeIcon icon={faCartShopping} />
+            </button>
         </div>
     );
 };
